c4af395a7555b7d5ba5cd6c84d825eb5e0d89c9e
Paige Seivertson <pzeig66@yahoo.com>
2021-03-30 02:28:34 -0400

    Remove unnecessary variable and func call

--- Host Gets Ingredients/TradeManager.lua
+++ Host Gets Ingredients/TradeManager.lua
@@ -6,15 +6,12 @@ function TradeManager:criminal_respawn(pos, rot, params)
     return
   end
 
-  local overrideChar = managers.criminals:character_data_by_peer_id(HGI.CustodyPeer:id())
-
   for equip, amount in pairs(managers.player._global.synced_equipment_possession[HGI.CustodyPeer:id()]) do
     if HGI:IsIngredient(equip) then
       HGIHelpers.PM:AddOrRemoveSpecial(managers.player, equip, true)
 
-      managers.hud:remove_teammate_special_equipment(overrideChar.panel_id, equip)
-      managers.network:session():send_to_peers_synched("sync_remove_equipment_possession", HGI.CustodyPeer:id(), equip)
       managers.player:remove_equipment_possession(HGI.CustodyPeer:id(), equip)
+      managers.network:session():send_to_peers_synched("sync_remove_equipment_possession", HGI.CustodyPeer:id(), equip)
     end
   end
 
