63d70cd71aa4274323599eef0b334f70d9a3992b
Paige Seivertson <pzeig66@yahoo.com>
2021-04-03 22:31:50 -0400

    Fix cooking removing all ingredients

--- Host Gets Ingredients/InteractionExt.lua
+++ Host Gets Ingredients/InteractionExt.lua
@@ -1,4 +1,5 @@
 local originalSelected = BaseInteractionExt.can_select
+local originalInteracted = UseInteractionExt.interact
 
 function BaseInteractionExt:can_select(player)
   if HGI:IsIngredient(self._tweak_data.special_equipment_block) then
@@ -6,4 +7,15 @@ function BaseInteractionExt:can_select(player)
   end
 
   return originalSelected(self, player)
+end
+
+function UseInteractionExt:interact(player)
+  if self._tweak_data.equipment_consume and HGI:IsIngredient(self._tweak_data.special_equipment) then
+    HGIHelpers.PM:AddOrRemoveSpecial(managers.player, self._tweak_data.special_equipment, false)
+
+    self._tweak_data.equipment_consume = nil
+    self._tweak_data.special_equipment = nil
+  end
+
+  return originalInteracted(self, player)
 end
